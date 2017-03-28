7bcf7287be038bfc832762111bce7aa8e1f90e2c
Anna Shelby <bonanashelby@gmail.com>
2017-03-28 16:46:40 -0700

    anna and erik worked on the index.html, server.js, routes.js and the articleController.js

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,10 @@
 'use strict';
 
+console.log('Hello World!');
+page('/', articleController.init);
+page('/about', aboutController.init);
+
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
-
+page();
 // TODO: What function do you call to activate page.js? Fire it off now, to execute
